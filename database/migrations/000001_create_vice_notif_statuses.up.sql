ccc1f6680fa04a59d7117553ad11e07de46550a9
Sarah Roberts <sarahr@cyverse.org>
2020-01-28 15:35:38 -0700

    updated the database migrations to work with Postgres 9.5

--- database/migrations/000001_create_vice_notif_statuses.up.sql
+++ database/migrations/000001_create_vice_notif_statuses.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS notif_statuses (
-	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	id UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
 	analysis_id UUID UNIQUE NOT NULL,
 	external_id UUID UNIQUE NOT NULL,
 	hour_warning_sent BOOL NOT NULL DEFAULT false,
